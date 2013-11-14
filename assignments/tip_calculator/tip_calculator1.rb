1f4a21884a792598fd8f0300a40bd10bb7e0ebed
Li Ming <limingth@gmail.com>
2013-11-14 10:15:31 -0800

    Modify with %.2f to control rounding

--- assignments/tip_calculator/tip_calculator1.rb
+++ assignments/tip_calculator/tip_calculator1.rb
@@ -18,8 +18,8 @@ meal_with_tax = meal + tax_value
 tip_value = meal_with_tax * tip
 total_cost = meal_with_tax + tip_value
 
-print "The pre-tax cost of your meal was $#{meal}\n"
-print "At #{tax*100}%, tax for this meal is $#{tax_value}\n"
-print "For a #{tip*100}% tip, you should leave $#{tip_value}\n"
-print "The grand total for this meal is then $#{total_cost}\n"
+print "The pre-tax cost of your meal was $%.2f\n" % meal
+print "At %d%%, tax for this meal is $%.2f\n" % [tax*100, tax_value]
+print "For a %d%% tip, you should leave $%.2f\n" % [tip*100, tip_value]
+print "The grand total for this meal is then $%.2f\n" % total_cost
 
